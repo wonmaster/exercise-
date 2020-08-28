b31f89bac49ae24db4fa5eb4cc648858ca80c58e
wonmaster <33542989+wonmaster@users.noreply.github.com>
2020-08-28 19:19:08 +0100

    Update test 9.py

--- test 9.py
+++ test 9.py
@@ -5,7 +5,7 @@ count= 0
 for line in fh :
     if not line.startswith('X-DSPAM-Confidence:'):
         continue
-    num = float(line.split(": ",1)[2])
+    num = float(line.split(": ",1)[1])
     tot = tot + num
     count +=1
 print ("Average spam confidence:",tot/count)
